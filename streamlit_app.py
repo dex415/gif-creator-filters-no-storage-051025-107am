5857981d4e4a3e43cc150384c40e5d14a4c33f95
dex415 <dexter@masvida.agency>
2025-05-10 03:10:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -134,7 +134,7 @@ if uploaded_files:
 
                 if add_watermark and os.path.exists(LOGO_PATH):
     logo = Image.open(LOGO_PATH).convert("RGBA")
-        base_width = img_cropped.width
+    base_width = img_cropped.width
     logo_width = int(base_width * (watermark_size / 100))
     w_percent = (logo_width / float(logo.size[0]))
     logo_height = int((float(logo.size[1]) * float(w_percent)))
